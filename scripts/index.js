625aa18183a1c38cdb760ca04e13ef472adedb8e
Zhenya Nikolaev <kindaboii@yandex.ru>
2022-11-24 15:54:07 +0300

    feat: develop initial cards and like buttons

--- scripts/index.js
+++ scripts/index.js
@@ -23,6 +23,40 @@ const editButton = page.querySelector('.profile__edit-btn');
 // Находим кнопку закрытия
 const popupToggle = page.querySelector('.popup__toggle');
 
+// Находим блок elements
+const elementsBlock = page.querySelector('.elements');
+
+// Находим темплейт карточки
+const cardTemplate = page.querySelector('#element-template').content;
+
+// Массив карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Судак',
+    link: 'https://images.unsplash.com/photo-1565342403875-07a8dc5ed13c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 
 //Вставляем значение из имени и работы в поля формы
 function inputTextInsert() {
@@ -42,7 +76,7 @@ function popupClose() {
 }
 
 // Обработчик «отправки» формы
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
 
   profileName.textContent = nameInput.value;
@@ -50,6 +84,23 @@ function formSubmitHandler (evt) {
 
   popupClose()
 }
+
+// Добавляем и заполняем карточки из массива
+initialCards.forEach(function (card) {
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+  cardElement.querySelector('.element__title').textContent = card.name;
+  cardElement.querySelector('.element__image').alt = card.name;
+  cardElement.querySelector('.element__image').src = card.link;
+
+  elementsBlock.append(cardElement);
+
+  const cardLikeButton = cardElement.querySelector('.element__like');
+
+  cardLikeButton.addEventListener('click', function(event){
+    event.target.classList.toggle('element__like_active')
+  });
+});
 
 
 // Прикрепляем обработчик к кнопке редактирования
@@ -60,3 +111,12 @@ popupToggle.addEventListener('click', popupClose);
 
 // Прикрепляем обработчик к кнопке сохранения
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+// 6 карточек из коробки
+
+
+
+
+
+
